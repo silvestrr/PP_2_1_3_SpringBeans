bc2d1a5a115db2d92a61469af8dfd1fa6c755bf7
silvestrr <nikitamaliev000@gmail.com>
2022-07-11 15:29:49 +0300

    new auto

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -4,10 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Cat extends Animal {
+public class Dog extends Animal {
 
-    @Override
     public String toString() {
-        return "Im a Cat";
+    return "Im a Dog";
     }
 }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,18 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
 
+
+
     public Long getTime() {
         return nanoTime;
     }
+
+
 }
